c0580edfabeb44ed162fffb838a23d2596b9291e
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-07-27 09:50:55 +0300

    Fixed faulty printing, added doxygen and C style comments

--- p3/main.c
+++ p3/main.c
@@ -5,19 +5,20 @@
 void progressCallback(int index, int dataSize);
 void handleData( void (*pCallback)(int, int), char* data, int dataSize );
 
+/*!
+ *  \fn  void handleData( void (*pCallback)(int, int), char* data, int dataSize )
+ *  \brief The function that handles the given task.
+ *  \param pCallback Function pointer that indicates toward the progressCallback function.
+ *  \param data Buffer with the string provided as entry data.
+ *  \param dataSize Size in bytes of the input data.
+ */
 void handleData( void (*pCallback)(int, int), char* data, int dataSize )
 {
     int i=0;
-    int j=0;
-    while(data[i]!='\0' && i<dataSize)
+    while(data[i]!='\0' && i<dataSize) /* either we reach the end of the string(\0), or the maximum limit of bytes. */
     {
-        while(j<=i)
-        {
-            printf("%c",data[j]);
-            ++j;
-        }
+        printf("%3d",data[i]);
         ++i;
-        j=0;
         /* this points to the progressCallback function
          * which will divide strlen(data) by the index (i) 
          * and return the percent.
@@ -30,34 +31,41 @@ void handleData( void (*pCallback)(int, int), char* data, int dataSize )
     }
 }
 
+/*!
+ *  \fn  void progressCallback(int index, int dataSize)
+ *  \brief Receives 2 ints and prints how much is 'index' of 'dataSize'
+ *  \param index The actual index of where the parsing process is at that very moment.
+ *  \param dataSize Length of the string entered as input.
+ */
 void progressCallback(int index, int dataSize)
 { 
-    // print the percent with only 2 decimals to avoid redundance
-    printf(" : %.2f%%\n", (float)index/(float)dataSize);
-    // we cast both ints to float to make sure that the result is realistic.
+    /* %6.2f to be alligned perfectly one under another */
+    printf(" | %6.2f%%\n", 100*(float)index/(float)dataSize);
 }
 
 
 
 int main(void)
 {
-    int dataSize = 0;
-    char* buffer = 0;
+    int dataSize = 0; /*! Wished data size */
+    char* buffer = 0; /*! char ptr ( buffer in which we will store the input string) */
     printf("Please give the size in bytes of the data you want to send.\n");
     printf("Keep in mind that one byte must be left for the \\0 special character ( string terminator ) :");
     scanf("%d",&dataSize);
 
-    // allocating memory for the char* depending on the size the user gave.
+    /* allocating memory for the char* depending on the size the user gave. */
     buffer = (char*)malloc(dataSize);
     if(buffer == 0)
     {
         fprintf(stderr,"Error when allocating the buffer.\n");
         exit(EXIT_FAILURE);
     }
-    // clear the buffer in order to avoid reading before we actually want it
+
+    /* clear the stdin buffer in order to avoid reading before we actually want it */
     getchar();
 
-    //reads the buffer from standard input (keyboard)
+    printf("\nEnter the data in the buffer: ");
+    /* reads the buffer from standard input (keyboard) */
     fgets(buffer,dataSize,stdin);
 
     /* if what we read has the size smaller than the buffer size we gave
