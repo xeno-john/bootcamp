bc50bf2dd1e986b5e8228131fe2df594fdc3f9f5
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-07-28 11:00:00 +0300

    followed coding guidelines; removed exit_failure; freed memory.

--- p3/main.c
+++ p3/main.c
@@ -2,85 +2,98 @@
 #include<stdlib.h>
 #include<string.h>
 
-void progressCallback(int index, int dataSize);
-void handleData( void (*pCallback)(int, int), char* data, int dataSize );
-
-/*!
- *  \fn  void handleData( void (*pCallback)(int, int), char* data, int dataSize )
- *  \brief The function that handles the given task.
- *  \param pCallback Function pointer that indicates toward the progressCallback function.
- *  \param data Buffer with the string provided as entry data.
- *  \param dataSize Size in bytes of the input data.
+/**
+ * @brief Function that receives two ints a prints a percentage.
+ * @param [in] index - index here the parser is in that moment
+ * @param [in] data_size - dimension of input buffer
  */
-void handleData( void (*pCallback)(int, int), char* data, int dataSize )
+void progress_callback(int index, int data_size);
+
+/**
+ * @brief Function that parses a char* character by character and prints the ascii value of it, also the progress made (%).
+ * @param [in] p_callback - pointer to the function that prints the percentage of the progress
+ * @param [in] input_data - char* where we have stocked our input data
+ * @param [in] data_size - dimension of input buffer
+ */
+void handle_data( void (*p_callback)(int, int), char* input_data, int data_size );
+
+void handle_data( void (*p_callback)(int, int), char* input_data, int data_size )
 {
     int i=0;
-    while(data[i]!='\0' && i<dataSize) /* either we reach the end of the string(\0), or the maximum limit of bytes. */
+
+    while ('\0' != input_data[i] && i<data_size) /* either we reach the end of the string(\0), or the maximum limit of bytes. */
     {
-        printf("%3d",data[i]);
+        printf("%3d",input_data[i]);
         ++i;
-        /* this points to the progressCallback function
-         * which will divide strlen(data) by the index (i) 
-         * and return the percent.
-         * We use strlen(data) and not dataSize
-         * because, even if the user gives the size to be a specific number
-         * the string he enters may be actually smaller in size.
-         * so the percent would not be realistic.
-         */
-        pCallback(i,strlen(data));
+        p_callback(i,strlen(input_data));
     }
+
 }
 
-/*!
- *  \fn  void progressCallback(int index, int dataSize)
- *  \brief Receives 2 ints and prints how much is 'index' of 'dataSize'
- *  \param index The actual index of where the parsing process is at that very moment.
- *  \param dataSize Length of the string entered as input.
- */
-void progressCallback(int index, int dataSize)
+void progress_callback(int index, int data_size)
 { 
     /* %6.2f to be alligned perfectly one under another */
-    printf(" | %6.2f%%\n", 100*(float)index/(float)dataSize);
+    printf(" | %6.2f%%\n", 100*(float)index/(float)data_size);
 }
 
 
 
 int main(void)
 {
-    int dataSize = 0; /*! Wished data size */
-    char* buffer = 0; /*! char ptr ( buffer in which we will store the input string) */
+    int          data_size = 0;
+    char*  input_buffer = NULL;
+    int    allocation_flag = 1;
+    int correct_input_flag = 1; 
+
     printf("Please give the size in bytes of the data you want to send.\n");
     printf("Keep in mind that one byte must be left for the \\0 special character ( string terminator ) :");
-    scanf("%d",&dataSize);
-
-    /* allocating memory for the char* depending on the size the user gave. */
-    buffer = (char*)malloc(dataSize);
-    if(buffer == 0)
+    
+    if (0 == scanf("%d",&data_size))
     {
-        fprintf(stderr,"Error when allocating the buffer.\n");
-        exit(EXIT_FAILURE);
+        correct_input_flag = 0;
+    }
+
+    input_buffer = (char*)malloc(data_size);
+    
+    if (NULL == input_buffer)
+    {
+        allocation_flag = 0;
     }
 
     /* clear the stdin buffer in order to avoid reading before we actually want it */
     getchar();
 
-    printf("\nEnter the data in the buffer: ");
-    /* reads the buffer from standard input (keyboard) */
-    fgets(buffer,dataSize,stdin);
-
-    /* if what we read has the size smaller than the buffer size we gave
-     * fgets will also read the \n(enter)
-     * so it needs to be remove explicitly.
-     */
-    if( buffer[strlen(buffer)-1] == '\n' )
+    if (1 == allocation_flag && 1 == correct_input_flag)
     {
-        buffer[strlen(buffer)-1] = '\0';
-    }
+        printf("\nEnter the data in the buffer: ");
+        
+        if (NULL != fgets(input_buffer,data_size,stdin))
+        {
 
-    /* & gives the adress of the respective function 
-     * so now the parameter will actually be an indicator to the 
-     * progressCallback function
-     */
-    handleData(&progressCallback,buffer,dataSize);
+            if ('\n' == input_buffer[strlen(input_buffer)-1])
+            {
+                input_buffer[strlen(input_buffer)-1] = '\0';
+            }
+
+        }
+        else
+        {
+            fprintf(stderr,"Reading the input data failed.\n");
+        }
+        
+
+        handle_data(&progress_callback,input_buffer,data_size);
+    }
+    else
+    {
+        fprintf(stderr,"Data entered in an ill manner.\n");
+    }
+    
+
+    if (1 == allocation_flag)
+    {
+        free(input_buffer);
+        input_buffer = NULL;
+    }
     return 0;
 }
