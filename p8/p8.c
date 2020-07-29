453a831515458064d5b8dc93bf61b7357b9106bb
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2020-07-29 15:44:19 +0300

    p8 and p9 updated

--- p8/p8.c
+++ p8/p8.c
@@ -2,16 +2,9 @@
 
 void *task(void *arg)
 {
-    // we explicitly cast the value to long (8 bytes) because
-    // the void pointer is 8 bytes in size
-    // and int type uses 4 bytes
+    long thread_id = (long)arg;
 
-    long tid = (long)arg;
-
-    // here we print the value we received.
-    DBG_PRINT(5,"Argument: %li\n",tid);
+    DBG_PRINT(5,"Argument: %li\n",thread_id);
     
-    // pthread_exit() function terminates the calling thread
-    // and retuns a value via retval
     pthread_exit(NULL);
 }
--- p9/main.c
+++ p9/main.c
@@ -3,44 +3,37 @@
 
 int main(void)
 {
-    // here we declare an array of (p)threads
     pthread_t threads[NUM_THREADS];
-    int i = 0; // contor value
+    int                      i = 0; 
+    int              *value = NULL;
 
-    int *value = 0; // the value that will be passed to the function in the threads
     value = (int*)malloc(sizeof(int)); // allocating memory for the pointer
     
-    if(value == NULL) // verify if the allocation was succesfull
+    if (NULL == value)
     {
         fprintf(stderr,"Error when allocating memory.\n");
         exit(EXIT_FAILURE); // if not, terminate the exectuion
     }
-
-    for(i = 0; i < NUM_THREADS; ++i)
+    else
     {
-        // pthread_create() function starts a new thread in 
-        // the calling process
-        if(pthread_create(&threads[i], NULL, task, (void*)value))
+
+        for(i = 0; i < NUM_THREADS; ++i)
         {
-            DBG_PRINT(1,"Error: can't create thread. %d\n",pthread_create(&threads[i], NULL, task, (void*)value));
-            exit(EXIT_FAILURE);
+            if(pthread_create(&threads[i], NULL, task, (void*)value))
+            {
+                DBG_PRINT(1,"Error: can't create thread. %d\n",pthread_create(&threads[i], NULL, task, (void*)value));
+                exit(EXIT_FAILURE);
+            }
         }
-    }
 
-    /* pthread_join() function waits for the speecified 
-     by thread to terminate.
-     if that thread has already terminated
-     pthread_join() returns immediately.
-     - Linux Programmer's Manual (man)
-    */
-    for(i = 0; i < NUM_THREADS; ++i)
-    {
-        pthread_join(threads[i],NULL);
-    }
+        for(i = 0; i < NUM_THREADS; ++i)
+        {
+            pthread_join(threads[i],NULL);
+        }
 
-    // deallocating the memory used for the value pointer.
-    free(value);
-    value = NULL;
+        free(value);
+        value = NULL;
+    }
 
     return 0;
 }
